04ccb579112fd4eaf465e87a94fb6af65ee21905
Jason Aviles <sandman2k18@outlook.com>
2019-04-19 20:10:37 -0400

    callbak.js line 59 change list[0] to list[i]

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -55,11 +55,12 @@ multiplyNums(4,5,(mul)=>{console.log(mul)})
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
- for (let i = 0; i < list.length; i++) {
-   return cb( item.includes('')===list[0]  ? true :false) 
+ let it = ()=>{for (let i = 0; i < list.length; i++) {
+   return list[i]===item  ? true :false;
    
- }
-
+   
+ }}
+  return cb(it())
 
  
 }
